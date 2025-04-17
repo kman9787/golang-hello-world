859ef640c5220ffe132dde994aa31a4a4cceb536
kman9787 <kazancheta87@gmail.com>
2025-04-17 10:55:04 +0800

    interfaces

--- main.go
+++ main.go
@@ -2,5 +2,5 @@ package main
 
 
 func main() { 
-	do_looping_struct()
+	do_myInterface()
 }
---
+++ myInterface.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name string
+	Breed string
+}
+
+
+type Gorrila struct {
+	Name string
+	Color string
+	NunberOfTheet int
+}
+
+func do_myInterface(){
+	dog := Dog {
+		Name: "Samson",
+		Breed: "German Shepherd",
+	}
+
+	PrintInfo(&dog)
+
+	gorilla := Gorrila{
+		Name: "Jock",
+		Color: "Blak",
+		NunberOfTheet: 38,
+	}
+
+	PrintInfo(&gorilla)
+}
+
+func PrintInfo (a Animal){
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "of legs")
+}
+
+func (d *Dog) Says() string{
+	return "Woof"
+}
+
+func (d *Dog) NumberOfLegs() int{
+	return 4
+}
+
+func (d *Gorrila) Says() string{
+	return "Ugh"
+}
+
+func (d *Gorrila) NumberOfLegs() int{
+	return 2
+}
